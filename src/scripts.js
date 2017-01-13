0e8441ac62149b456af91e2da7d29148554c1d25
Shawn Erquhart <shawn@professant.com>
2017-01-13 16:03:48 -0500

    wip - docs nav

--- src/scripts.js
+++ src/scripts.js
@@ -35,3 +35,78 @@ document.getElementById('docs').addEventListener('click', event => {
     }
   }
 });
+
+
+// Sticky nav
+const stickyData = {};
+let stickyElements = document.getElementsByClassName('js-sticky');
+var largeScreenMediaQuery = '(min-width: 700px)';
+
+responsiveInit();
+
+window.addEventListener('resize', responsiveInit);
+
+function responsiveInit() {
+  var viewportIsLarge = window.matchMedia(largeScreenMediaQuery).matches;
+  if (viewportIsLarge && !stickyData.viewportWasLarge) {
+    initForLargeViewport();
+    stickyData.viewportWasLarge = true;
+    stickyData.initialized = true;
+  } else if (viewportIsLarge) {
+    destroyStickies();
+    initForLargeViewport();
+  } else if (!viewportIsLarge && (stickyData.viewportWasLarge || !stickyData.initialized)) {
+    teardownForLargeViewport();
+    stickyData.viewportWasLarge = false;
+    stickyData.initialized = true;
+  }
+}
+
+function initForLargeViewport() {
+  stickyInit(stickyElements);
+  handleStickies();
+  window.addEventListener('scroll', handleStickies);
+}
+
+function teardownForLargeViewport() {
+  window.removeEventListener('scroll', handleStickies);
+  destroyStickies();
+}
+
+function stickyInit(stickyElements) {
+  stickyData.stickyElements = [];
+  for (let i = 0; i < stickyElements.length; i++) {
+    stickyData.stickyElements.push({
+      element: stickyElements[i],
+      originalTop: stickyElements[i].getBoundingClientRect().top + window.pageYOffset
+    });
+  }
+}
+
+function handleStickies() {
+  let top = window.pageYOffset;
+
+  stickyData.stickyElements.forEach(function (elementObj) {
+    if (!elementObj.stickyActive && top >= elementObj.originalTop) {
+      activateSticky(elementObj);
+    } else if (elementObj.stickyActive && top < elementObj.originalTop) {
+      deactivateSticky(elementObj);
+    }
+  });
+}
+
+function destroyStickies() {
+  if (stickyData.stickyElements) {
+    stickyData.stickyElements.forEach(deactivateSticky);
+  }
+}
+
+function activateSticky(elementObj) {
+  elementObj.element.classList.add('js-sticky-active');
+  elementObj.stickyActive = true;
+}
+
+function deactivateSticky(elementObj) {
+  elementObj.element.classList.remove('js-sticky-active');
+  elementObj.stickyActive = false;
+}
